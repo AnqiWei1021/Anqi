b7daf0a97180b21c05e9666fa302d4a9b8ada28a
AnqiWei1021 <fm7hhqgxtt@privaterelay.appleid.com>
2025-12-03 00:47:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 const text = "I think therefore I am.";
 const container = document.getElementById("text-container");
 
-text.split("").forEach((char, index) => {
-    const block = document.createElement("span");
-    block.className = "letter-block";
+text.split("").forEach(char => {
 
-    const letter = document.createElement("span");
-    letter.className = "letter";
-    letter.textContent = char;
+    if (char === " ") {
+        // 空格部分给一个水平间隔
+        const space = document.createElement("div");
+        space.style.width = "20px";
+        container.appendChild(space);
+        return;
+    }
+
+    const block = document.createElement("div");
+    block.className = "letter-block";
 
     const line = document.createElement("div");
     line.className = "line";
 
-    let height = Math.floor(Math.random() * 180) + 80; // 长短不一
-    line.style.height = height + "px";
+    // 线条长短不一（但差别不会太夸张）
+    const randomLength = 60 + Math.random() * 120; 
+    line.style.height = randomLength + "px";
+
+    const letter = document.createElement("div");
+    letter.className = "letter";
+    letter.textContent = char;
 
     block.appendChild(line);
     block.appendChild(letter);
